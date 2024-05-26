500f12b51b90bdd3a69a48595c44473cb600d022
kanth-g <kanthgandasiri@gmail.com>
2024-05-26 11:06:00 +0530

    inital commit

---
+++ build/js/example/mainEx1.js
@@ -0,0 +1,9 @@
+"use strict";
+let userName = "Srikanth";
+console.log(userName);
+/* for run ts file use command :- tsc <file name>  or tsc <filename> -w (watch updates on the file)
+    when you run this command it will genarate one js file with old(es5) javascricpt complation code
+    means not comatable with modren browsers to over come this we created one file known as config.js file
+
+    using config.js file set root directory ts file and set store complied file
+*/
